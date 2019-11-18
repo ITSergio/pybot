1a12e5daba3d483f648337eaafe6719ba3bd1a7c
ITSergio <33862930+ITSergio@users.noreply.github.com>
2019-11-18 22:27:01 +0200

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -1,11 +1,14 @@
-def get_chat_id(update):  
-    chat_id = update['message']['chat']['id']
-    return chat_id
+import requests
 
-def send_mess(chat, text):  
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
+url = "https://api.telegram.org/bot<ваш_токен>/"
 
-chat_id = get_chat_id(last_update(get_updates_json(url)))
-send_mess(chat_id, 'Your message goes here')
+
+def get_updates_json(request):  
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+
+def last_update(data):  
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
