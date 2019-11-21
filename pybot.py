a983136cc31d9680f8700bc49ab58de672c95ca6
ITSergio <33862930+ITSergio@users.noreply.github.com>
2019-11-21 20:55:34 +0200

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -1,14 +1,9 @@
-import requests
+import telebot
 
-url = "https://api.telegram.org/bot1062798887:AAHGlmS2tJDkOiag3boqZ4re8Fo2zFblhX8/"
-
-
-def get_updates_json(request):  
-    response = requests.get(request + 'getUpdates')
-    return response.json()
+bot =telebot.TeleBot('1062798887:AAHGlmS2tJDkOiag3boqZ4re8Fo2zFblhX8')
 
 
-def last_update(data):  
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Привіт, я Telegram бот")
+      bot.polling()
