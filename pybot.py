88fc72f893bfc1a0f8634462ffd8f2b6ccc8bb68
ITSergio <33862930+ITSergio@users.noreply.github.com>
2019-11-21 20:56:55 +0200

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -5,5 +5,5 @@ bot =telebot.TeleBot('1062798887:AAHGlmS2tJDkOiag3boqZ4re8Fo2zFblhX8')
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, "Привіт, я Telegram бот")
+    bot.send_message(message.chat.id, "Привіт, бот")
       bot.polling()
